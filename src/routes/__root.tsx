8fd8425ff1e8d428fa34ecba2436b3e3ace57722
zxlvera <xdoki26@gmail.com>
2024-11-11 00:02:25 +0800

    update tanstack router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,17 @@
+import { lazy } from "react"
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      )
 
 export const Route = createRootRoute({
   component: () => (
--- src/routes/buttons.tsx
+++ src/routes/buttons.tsx
@@ -11,7 +11,7 @@ function Buttons() {
     <div className="relative overflow-hidden bg-white">
       <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
         <div className="mx-auto flex max-w-7xl flex-col items-center justify-center px-4 sm:static sm:px-6 lg:px-8">
-          <div className="grid w-fit grid-cols-[repeat(auto-fit,_minmax(400px,_1fr))] items-center gap-4">
+          <div className="grid w-fit grid-cols-[repeat(auto-fit,_minmax(400px,_1fr))] items-center gap-8">
             <div className="flex flex-wrap items-center gap-8">
               <Button variant={"primary"} size="md" label={"Button CTA"} />
               <Button
